3dfa818f66fb3455f2c1c1776c5022f1446652c8
wenjia <dwgia027@gmail.com>
2022-11-16 10:40:07 +1100

    done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Follow along with the examples here
+
+// const sum = add(1, 2);
+
+const message = `The sum of your numbers is: ${add(1, 2)}`;
+console.log(message);
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+}
+
+console.log(say("hi", "wenjia"));
